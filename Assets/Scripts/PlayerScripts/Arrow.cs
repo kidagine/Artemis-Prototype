dad91647b93995a3efa0b0c846af92d1386a6d5e
unknown <davidkalatzis@gmail.com>
2020-02-21 06:15:56 +0100

    Improved the interaction with the enemy by adding more effects and feedback to the player, and also introducing a few simple mechanics.

--- Assets/Scripts/PlayerScripts/Arrow.cs
+++ Assets/Scripts/PlayerScripts/Arrow.cs
@@ -6,11 +6,14 @@ public class Arrow : MonoBehaviour
     [SerializeField] private ParticleSystem hitEffectParticleSystem;
     [SerializeField] private ParticleSystem catchParticleSystem;
     [SerializeField] private Player player;
+    [SerializeField] private Animator reticleAnimator;
     [SerializeField] private Transform summonPoint;
     [SerializeField] private Material[] materials;
-    private float summonSpeed = 4;
-    private const int summonMultiplier = 20;
+    private GameObject stuckToEnemy;
+    private float summonSpeed = 6;
+    private const int summonMultiplier = 45;
     private bool isSummoned;
+    private bool hasHit;
     private const int rotateToTargetSpeed = 10;
 
 
@@ -22,13 +25,17 @@ public class Arrow : MonoBehaviour
     void Update()
     {
         Summon();
-
     }
     
     private void Summon()
     {
         if (isSummoned)
         {
+            if (stuckToEnemy != null)
+            {
+                stuckToEnemy.GetComponent<IEnemy>().Die();
+                stuckToEnemy = null;
+            }
             float distance = Vector3.Distance(summonPoint.position, transform.position);
             if (distance < 1)
             {
@@ -49,7 +56,7 @@ public class Arrow : MonoBehaviour
 
                 foreach (Material material in materials)
                 {
-                    material.SetFloat("_GlowPower", 0.03f);
+                    material.SetFloat("_GlowPower", 0.08f);
                 }
             }
         }
@@ -64,6 +71,7 @@ public class Arrow : MonoBehaviour
 
     private void PickUp()
     {
+        hasHit = false;
         isSummoned = false;
         player.GrabArrow();
         summonSpeed = 2;
@@ -83,23 +91,40 @@ public class Arrow : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        IInteractable interactable = other.gameObject.GetComponent<IInteractable>();
-        IEnemy enemy = other.gameObject.GetComponent<IEnemy>();
+        if (!arrowRigidbody.isKinematic)
+        {
+            IInteractable interactable = other.gameObject.GetComponent<IInteractable>();
+            IEnemy enemy = other.gameObject.GetComponent<IEnemy>();
 
-        if (!isSummoned)
-        {
-            AudioManager.Instance.Play("BounceArrow");
-            hitEffectParticleSystem.Play();
-        }
-        if (interactable != null)
-        {
-            interactable.Interact();
-        }
-        if (enemy != null)
-        {
-            Vector3 hitDirection = other.contacts[0].point - transform.position;
-            hitDirection = -hitDirection.normalized;
-            enemy.Damage(hitDirection);
+            if (!isSummoned && !hasHit)
+            {
+                AudioManager.Instance.Play("BounceArrow");
+                hitEffectParticleSystem.Play();
+            }
+            if (interactable != null)
+            {
+                interactable.Interact();
+            }
+            if (enemy != null && !hasHit)
+            {
+                AudioManager.Instance.Play("Kill");
+                reticleAnimator.SetTrigger("Hit");
+                hasHit = true;
+                Vector3 hitDirection = other.contacts[0].point - transform.position;
+                hitDirection = -hitDirection.normalized;
+                int enemyhealth = enemy.Damage(hitDirection);
+                if (enemyhealth == 0)
+                {
+                    foreach (Material material in materials)
+                    {
+                        material.SetFloat("_GlowPower", 0.08f);
+                    }
+                    stuckToEnemy = other.collider.gameObject;
+                    arrowRigidbody.Sleep();
+                    arrowRigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+                    arrowRigidbody.isKinematic = true;
+                }
+            }
         }
     }
 }
